3fe48f94a16af962de873d927abe14b9a99d7731
Arthur Ydalgo <arthurc@alunos.utfpr.edu.br>
2021-07-14 09:29:06 -0300

    Update client.php

--- client.php
+++ client.php
@@ -1,12 +1,12 @@
 // Syncs clock
 // http://worldtimeapi.org/api/timezone/America/Sao_Paulo
 
-// Reads data
-
-// Encrypts data
-
 // Generates TOTP-Token
 
 // Creates packet
 
 // Sends packet
+
+// Waits for reply
+
+// Displays returned data (or error)
