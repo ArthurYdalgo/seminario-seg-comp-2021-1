b8e432e5373577d6cf53024031e1b36e89bed5ed
Arthur Ydalgo <arthurc@alunos.utfpr.edu.br>
2021-07-14 09:27:53 -0300

    Update and rename server.php to api.php

---
+++ api.php
@@ -0,0 +1,30 @@
+<?
+$mock_data = [
+    0 => [
+        'name' => 'Fulano',
+        'birthday' => '1997-09-07'
+    ],
+    1 => [
+        'name' => 'Ciclano',
+        'birthday' => '1996-10-08'
+    ],
+    2 => [
+        'name' => 'Beltrano',
+        'birthday' => '1995-11-09'
+    ],
+    3 => [
+        'name' => 'Loki',
+        'birthday' => '1610-04-01'
+    ],
+];
+
+// Syncs clock
+// http://worldtimeapi.org/api/timezone/America/Sao_Paulo
+
+// Listens for incoming packets
+
+// Generates TOTP-Token for validation
+
+// Checks TOTP-Token
+
+// Returns (or not) requested data
